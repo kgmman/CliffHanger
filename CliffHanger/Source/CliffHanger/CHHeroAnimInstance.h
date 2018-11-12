e58e3a851dd0d9d9cc351430a73bb04b412c7c3d
kgmman <kgmman@naver.com>
2018-11-13 00:23:44 +0900

    .

---
+++ CliffHanger/Source/CliffHanger/CHHeroAnimInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CHHeroAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLIFFHANGER_API UCHHeroAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
